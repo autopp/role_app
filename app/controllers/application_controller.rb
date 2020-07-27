0d1d4523a0c89808d44f29a206fe3a1f265d566d
autopp <autopp.inc@gmail.com>
2020-07-27 22:07:36 +0900

    feat: rescue pundit error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = 'not allowed'
+    redirect_to(request.referer || root_path)
+  end
 end
